6a2b4bf65ddab1bb58d6c87480f3d3ea8ad377c5
Cody Oakes <codyoakes@me.com>
2020-04-16 16:37:20 -0600

    setup test database env

---
+++ src/playground/promises.js
@@ -0,0 +1,29 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({
+      name: 'Andrew',
+      age: 26,
+    })
+  }, 1500)
+})
+
+console.log('before')
+
+promise
+  .then((data) => {
+    console.log('1', data)
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('this is other promise')
+      }, 5000)
+    })
+  })
+  .then((str) => {
+    console.log('does this run? ', str)
+  })
+  .catch((error) => {
+    console.error('error: ', error)
+  })
+
+console.log('after')
