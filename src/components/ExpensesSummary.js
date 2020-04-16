1a947d1086671f536f55417fa199470f696ce01a
Cody Oakes <codyoakes@me.com>
2020-04-15 20:14:10 -0600

    refactor ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,28 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import numeral from 'numeral'
+
 import getExpensesTotal from '../selectors/expenses-total'
 import selectExpenses from '../selectors/expenses'
 
-export const ExpensesSummary = (props) => {
-  const expenseCount = props.expenses.length
-  const expenseTotal = getExpensesTotal(props.expenses)
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const formattedExpenseTotal = numeral(expensesTotal / 100).format('$0,0.00')
   return (
     <div>
-      {expenseCount === 0 ? (
-        <p>no summary</p>
-      ) : (
-        <p>
-          Viewing {expenseCount} expense(s) totalling{' '}
-          {numeral(expenseTotal / 100).format('$0,0.00')}
-        </p>
-      )}
+      <h3>
+        Viewing {expenseCount} expense(s) totalling {formattedExpenseTotal}
+      </h3>
     </div>
   )
 }
 
-const mapStateToProps = (state) => ({
-  expenses: selectExpenses(state.expenses, state.filters),
-})
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters)
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: getExpensesTotal(visibleExpenses),
+  }
+}
 
 export default connect(mapStateToProps)(ExpensesSummary)
